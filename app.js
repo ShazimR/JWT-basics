9e791b09ba3c0bc16c14e019da1c3d873e6924e6
Shazim Rahman <shazimrahman@hotmail.com>
2023-07-04 18:36:19 -0400

    add jwt authentication and authorization

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -11,14 +12,18 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.static('./public'));
 app.use(express.json());
 
+app.use('/api/v1', mainRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 
+const port = process.env.PORT || 3000;
+
 const start = async () => {
   try {
-    app.listen(process.env.PORT, process.env.HOST,() =>
-      console.log(`Server is listening at: http://${process.env.HOST}:${process.env.PORT}...`)
+    app.listen(port, process.env.HOST,() =>
+      console.log(`Server is listening at: http://${process.env.HOST}:${port}...`)
     );
   } catch (error) {
     console.log(error);
